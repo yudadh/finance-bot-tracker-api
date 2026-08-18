60aca1f24dc5b6bc66b5a73aa05e0bd975847360
YudaDh <yudadarmawan26@gmail.com>
2026-08-18 10:04:13 +0800

    fix: correct typo on field name and query

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -12,17 +12,17 @@ const (
 
 type User struct {
 	ID               uint64        `gorm:"column:id;primaryKey"`
-	TelegramID       uint64        `gorm:"column:telegram_id;unique"`
+	TelegramID       int64         `gorm:"column:telegram_id;unique"`
 	TelegramUsername string        `gorm:"column:telegram_username"`
 	FirstName        string        `gorm:"column:first_name"`
 	LastName         string        `gorm:"column:last_name"`
 	LanguageCode     string        `gorm:"column:language_code"`
 	Timezone         string        `gorm:"column:timezone"`
 	Status           UserStatus    `gorm:"column:status"`
-	LastSeenAt       *time.Time     `gorm:"column:last_seen_at"`
+	LastSeenAt       *time.Time    `gorm:"column:last_seen_at"`
 	CreatedAt        time.Time     `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt        time.Time     `gorm:"column:updated_at;autoCreateTime;autoUpdateTime"`
-	Budgets          []Budget      `gorm:"foreignKey:user_id;references:id"`
+	Budgets          []Budget      `gorm:"foreignKey:UserID;references:ID"`
 	Transactions     []Transaction `gorm:"foreignKey:UserID;references:ID"`
 	ActivityLogs     []ActivityLog `gorm:"foreignKey:UserID;references:ID"`
 }
