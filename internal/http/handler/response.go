45c459b5194921f7e23e2de42d49c6a5854177e0
YudaDh <yudadarmawan26@gmail.com>
2026-09-02 15:42:21 +0800

    feat: create a global handling function to ensure consistent responses and centralized error logging

---
+++ internal/http/handler/response.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type HandlerFunc func(c *gin.Context) error
+
+type Response struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
+}
+
+func Handle(logger *slog.Logger, fn HandlerFunc) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		if err := fn(ctx); err != nil {
+			HandleError(ctx, err)
+
+			if ctx.Writer.Status() >= http.StatusInternalServerError {
+				logger.ErrorContext(ctx.Request.Context(),
+					"http request failed",
+					"method", ctx.Request.Method,
+					"path", ctx.Request.URL.Path,
+					"status", ctx.Writer.Status(),
+					"err", err, 
+				)
+			}
+		}
+	}
+}
+
+func ResponseSuccess(c *gin.Context, statusCode int, message string, data any) {
+	c.JSON(statusCode, Response{
+		Error:   false,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func ResponseError(c *gin.Context, statusCode int, message string, fieldErrors []ValidationFieldError) {
+	c.JSON(statusCode, ErrorResponse{
+		Error:   true,
+		Message: message,
+		Errors: fieldErrors,
+	})
+}
